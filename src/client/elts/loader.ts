46967780985cdcdb2011921e1603494a58e35a42
Daniel Bucci <dlbucci@superftc.com>
2025-09-09 11:30:54 -0700

    Update Loading to show favicon

--- src/client/elts/loader.ts
+++ src/client/elts/loader.ts
@@ -1,8 +1,11 @@
+import { size as sizeAttr, src } from "rokay/browser/attr"
 import { apd } from "rokay/browser/core"
-import { div, h2 } from "rokay/browser/elt"
+import { div, img } from "rokay/browser/elt"
 import { match } from "rokay/browser/match"
+import { alignItems, justifyContent, left, size, top } from "rokay/browser/style"
 import { Asink, Async } from "rokay/prop/async"
 import { PropView } from "rokay/prop/prop"
+import { $absolute, $flexRow } from "style/utils.gen"
 
 import { ErrorDisplay } from "./error-display"
 
@@ -11,16 +14,22 @@ export const
   Loader = <T>(asink: Asink<T>, render: (t: T) => Node | undefined) =>
     LoaderProp(asink.prop, render),
 
-  LoaderProp = <T>(prop: PropView<Async<T>>, render: (t: T) => Node | undefined) =>
-    div(apd(
-      match(prop, (prop) =>
-        prop.t === "load" ?
-          h2(apd("Loading..."))
-        : prop.t === "error" ?
-          ErrorDisplay(prop.error)
-        :
-          render(prop.data)
-      ),
-    ))
-
-
+  LoaderProp = <T>(prop: PropView<Async<T>>, render: (t: T) => Node | undefined) => div(
+    apd(match(prop, (prop) =>
+      prop.t === "load" ?
+        div(
+          alignItems("center"),
+          $flexRow,
+          justifyContent("center"),
+          $absolute,
+          top("0"),
+          left("0"),
+          size("100%"),
+          apd(img(src("/art/icons/16.png"), sizeAttr(16), size("256px"))),
+        )
+      : prop.t === "error" ?
+        ErrorDisplay(prop.error)
+      :
+        render(prop.data)
+    )),
+  )
--- src/server/html.ts
+++ src/server/html.ts
@@ -1,28 +1,40 @@
 import { MixArgs } from "rokay/mix"
-import { async, charset, lang, src } from "rokay/server/attr"
+import { async, charset, lang, size as sizeAttr, src } from "rokay/server/attr"
 import { apd, Elt } from "rokay/server/core"
-import { _meta, body, head, html, link, meta, script, title } from "rokay/server/elt"
+import { _meta, body, div, head, html, img, link, meta, script, title } from "rokay/server/elt"
+import { alignItems, display, justifyContent, size } from "rokay/server/style"
 
 import { AppServer } from "./app.js"
 
 
 export const
-  HTML = ({}: AppServer, ...args: MixArgs<Elt>) => html(lang("en"), apd(
-    head(apd(
-      title(apd("Kiki")),
+  HTML = ({}: AppServer, ...args: MixArgs<Elt>) => html(
+    lang("en"),
+    apd(
+      head(apd(
+        title(apd("Kiki")),
 
-      _meta(charset("utf-8")),
-      meta("mobile-web-app-capable", "yes"),
-      meta("apple-mobile-web-app-capable", "yes"),
-      meta("format-detection", "telephone=no"),
-      meta("theme-color", "#000"),
-      meta("viewport", "initial-scale=1,user-scalable=no,width=device-width"),
+        _meta(charset("utf-8")),
+        meta("mobile-web-app-capable", "yes"),
+        meta("apple-mobile-web-app-capable", "yes"),
+        meta("format-detection", "telephone=no"),
+        meta("theme-color", "#000"),
+        meta("viewport", "initial-scale=1,user-scalable=no,width=device-width"),
 
-      link("icon", "/art/icons/16.png"),
-      link("manifest", "/manifest.json"),
-      link("stylesheet", "/kiki.css"),
+        link("icon", "/art/icons/16.png"),
+        link("manifest", "/manifest.json"),
+        link("stylesheet", "/kiki.css"),
 
-      script(async, src("/kiki.js")),
-    )),
-    body(...args),
-  ))
+        script(async, src("/kiki.js")),
+      )),
+      body(
+        apd(div(
+          alignItems("center"),
+          display("flex"),
+          justifyContent("center"),
+          apd(img(src("/art/icons/16.png"), sizeAttr(16), size("64px"))),
+        )),
+        ...args
+      ),
+    ),
+  )
